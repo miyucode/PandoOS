ffbbb6335a3fce7793f5673ae1f957e50cbb5e81
MiyuCode <streamlabsdonationtools@gmail.com>
2021-06-05 16:46:04 +0200

    Fix "boot.py" 🔨

--- boot.py
+++ boot.py
@@ -16,37 +16,20 @@ class res(object):
     def __init__(res, arg):
         super(res, self).__init__()
         self.arg = arg
-    boot = "no"
     version = "1.2"
 
 # Boot (start)
 
-try:
-    if res.boot == "yes":
-        print("PandoOS are already launched.")
-        os.system('ping localhost>nul')
-        os.system('exit')
-    else:
-        pass
-
 def boot():
-    if res.boot == "no":
-        res.boot = "yes"
-        session = open("sessions/sessions.PandoOS-session", "w+")
-        session.write("session=user")
-        session.close()
-        mb.showinfo("PandoOS","Boot successful, press ENTER for continue.")
-        root.destroy()
-        os.system('cls')
-        print("PandoOS> Boot...")
-        print(f"PandoOS> Latest version: {res.version}")
-        os.system('py PandoOS.py')
-    elif res.boot == "yes":
-        os.system('exit')
-
-# Run
-
-res.boot = "no"
+    session = open("sessions/sessions.PandoOS-session", "w+")
+    session.write("session=user")
+    session.close()
+    mb.showinfo("PandoOS","Boot successful, press ENTER for continue.")
+    root.destroy()
+    os.system('cls')
+    print("PandoOS> Boot...")
+    print(f"PandoOS> Latest version: {res.version}")
+    os.system('py PandoOS.py')
 
 # UI
 
