d5c021ee44298ebe001c92788bea8bbc20eae2ad
MiyuCode <streamlabsdonationtools@gmail.com>
2022-03-05 09:39:52 +0100

    boot: remake boot function

--- boot.py
+++ boot.py
@@ -1,22 +1,9 @@
-from tkinter import *
-from tkinter.ttk import *
-import tkinter.messagebox as mb
 import os
-import random
-
-root = Tk()
-root.title("Welcome To PandoOS")
-root.geometry("300x100")
-root.iconbitmap("img/logo.ico")
-root.bind("<Escape>", lambda e: root.destroy())
-root.resizable(False, False)
-root.withdraw()
 
 # Boot (start)
 
 def boot():
     os.system('cls')
-    mb.showinfo("PandoOS","Démarrage effectué, appuyez sur ENTRER pour continuer.")
     # root.destroy()
     os.system('cls')
     print("PandoOS> Lancement...")
@@ -24,12 +11,3 @@ def boot():
     os.system(pandoos)
 
 boot()
-
-# UI
-
-# Label(root, text="\n").pack()
-# boot = Button(root, text="Boot PandoOS", command=boot, width=30)
-# boot.pack(anchor='center')
-# boot.grid()
-
-# root.mainloop()
